e3b02d465539c15323cd8ce39e200f4d0aa777a9
St.limp <st.limp@mail.ru>
2015-08-09 02:41:54 +0200

    Contact layout implementation started. Yii2 debugger updated(?)

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,15 +1,39 @@
 <?php
 use yii\helpers\Html;
-
-/* @var $this yii\web\View */
-$this->title = 'О компании';
 ?>
 <div class="site-index">
    <div class="jumbotron">
- 
-    <p>
-       Страница Контакты
-    </p>
+	<div class="left-half">
+		<div class="left-side-half-header">
+		КОНТАКТЫ 
+		</div>
+		<div class="left-side-half-text">
+			<p><img src="images/content/logomonochrome.jpg" align="left">ООО «ГЕККОСТОУН», УНП 290489059
+				<br>
+	юридический адрес: д. Черни, ул. Брестская 56/6;  Брестская область.</p>
+		</div>
+		<div class="left-side-half-header">
+		СХЕМА ПРОЕЗДА НА СКЛАД-МАГАЗИН ДЕКОРАТИВНОГО КАМНЯ 
+		</div>
+		<div class="left-side-half-text">
+
+		</div>
+		<div class="left-side-half-header">
+		ФОРМА ОБРАТНОЙ СВЯЗИ 
+		</div>
+		<div class="left-side-half-text">
+		</div>
+	</div>
+	<div class="right-half">
+		<div class="right-side-half-header">
+			ФОРМА ДЛЯ СВЯЗИ
+		</div>
+		<div class="right-side-half-text">
+			<p></p>
+		</div>
+	</div>
 	</div>
 </div>
+
+
 
