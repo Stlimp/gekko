67bdcc8a5ea7f20544fe5cd2f3859e0017940d2a
St.limp <st.limp@mail.ru>
2015-10-01 22:16:00 +0300

    Menu hover small fix

--- views/layouts/indexLayout.php
+++ views/layouts/indexLayout.php
@@ -170,13 +170,14 @@ echo Carousel::widget([
 
 <script type="text/javascript" class="noprint">
     $(document).ready(function(){
-    /*Menu hover. Replaced css way ov hover*/
-    $('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-     }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-    /*Menu hover. Replaced css way ov hover*/
-});
+        /*Menu hover. Replaced css way ov hover*/
+        $('ul.nav li.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+             }, function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+            /*Menu hover. Replaced css way ov hover*/
+        });
+    });
 </script> 
 </html>
 <?php $this->endPage() ?>
