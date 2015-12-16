9a08e4b95f4d86ecf01bd0dc0141b320a973409f
St.limp <st.limp@mail.ru>
2015-12-16 23:40:43 +0300

    Index arrow up z-index fixed

--- views/site/index.php
+++ views/site/index.php
@@ -138,8 +138,7 @@ $this->registerJsFile('@web/js/lightSlider.js', ['depends' => [\yii\web\JqueryAs
   
 <div class="site-index">
     <div class="jumbotron">
-             
-        <div class="col-md-12 hover-slide text-center" id="scroll" style="text-align:right;bottom: -100px;z-index: 1015";> <a href="#upper" ><div class="button-up"></div> </a>   </div>     
+        <div class="col-md-12 hover-slide text-center" id="scroll" style="height:0px;text-align:right;bottom: -100px;";> <a href="#upper" ><div class="button-up"></div> </a>   </div>     
         <div class="page-text">
         <div class="left_third" style= <?= "width:275px;background-image:url(&quot;".$product->product_product_image."&quot;);background-size:cover" ?>>
             <span class="left_product_name" >
@@ -148,48 +147,57 @@ $this->registerJsFile('@web/js/lightSlider.js', ['depends' => [\yii\web\JqueryAs
         </div>
         <div class="center_third">
             <div class="center_split">
-               <div class="center_split_vertically">
-                   <p>ПОДБОР КАМНЯ</p>
-               </div>
+               <a href="#">
+                    <div class="center_split_vertically">
+                       <p>ПОДБОР КАМНЯ</p>
+                    </div>
+                </a>
                <div class="center_split_text"><p>При помощи конфигуратора «ПОДБОР КАМНЯ» 
                     Вы сможете самостоятельно подобрать самую 
                     оптимальную фактуру и расцветку декоративного
                     камня для интерьера или экстерьера Вашего дома.</p>
                 </div>
             </div>
+            
             <div class="center_split">
+                <a href="#">
                 <div class="center_split_vertically">
                    <p>ДО И ПОСЛЕ</p>
-               </div>
-
-               <div class="center_split_text"><p>В фотогалерее представлены фотографии интерьеров
-                    и экстерьеров ДО и ПОСЛЕ проведения облицовочных 
-                    работ по укладке декоративного камня.</p> 
+                </div>
+                </a>    
+                <div class="center_split_text"><p>В фотогалерее представлены фотографии интерьеров
+                        и экстерьеров ДО и ПОСЛЕ проведения облицовочных 
+                        работ по укладке декоративного камня.</p> 
                 </div>
             </div>
-            <div class="center_split" style="margin-bottom:0px">
-                <div class="center_split_vertically">
-                   <p>ВИДЕОГАЛЛЕРЕЯ</p>
-               </div>
-               <div class="center_split_text"><p>В видеогалерее представлены демонстрационные,
-                а также обучающие видео по применению в облицовке
-                декоративного облицовочного камня.</p>
+            
+            <a href="">
+                <div class="center_split" style="margin-bottom:0px">
+                    <a href="index.php?r=videogallery%2Findex"><div class="center_split_vertically" style="background-image:url(images/content/videogallery.jpg);background-size:cover;">
+                       <p>ВИДЕОГАЛЛЕРЕЯ</p>
+                   </div></a>
+                   <div class="center_split_text"><p>В видеогалерее представлены демонстрационные,
+                    а также обучающие видео по применению в облицовке
+                    декоративного облицовочного камня.</p>
+                    </div>
                 </div>
-            </div>
-
+            </a>
         </div>
         <div class="right_third">
-            <div class="right_split">
-                <span class="test_text">НАТУРАЛЬНЫЙ ИЛИ ИСКУСТВЕННЫЙ?</span>
-                <p style="font-family:Corbel"><b>ТЕСТ.</b> Попробуй отыскать натуральные камни среди искусственных камней GEKKOSTONE!</p>
-            </div>
-            <div class="right_split">
-                <p style="font-family:Corbel">Собери собственную галерею из наиболее понравившихся фактур 
-и расцветок!</p>
-                <span class="test_text">МОЯ ГАЛЕРЕЯ</span>
-                <p style="font-family:Corbel">Расчет необходимого количества камня, его веса и стоимости!</p>
-
-            </div>
+            <a href="#">
+                <div class="right_split">
+                    <span class="test_text">НАТУРАЛЬНЫЙ ИЛИ ИСКУСТВЕННЫЙ?</span>
+                    <p style="font-family:Corbel"><b>ТЕСТ.</b> Попробуй отыскать натуральные камни среди искусственных камней GEKKOSTONE!</p>
+                </div>
+            </a>
+            <a href="index.php?r=site%2Fcart">
+                <div class="right_split">
+                    <p style="font-family:Corbel">Собери собственную галерею из наиболее понравившихся фактур 
+    и расцветок!</p>
+                    <span class="test_text">МОЯ ГАЛЕРЕЯ</span>
+                    <p style="font-family:Corbel">Расчет необходимого количества камня, его веса и стоимости!</p>
+                </div>
+            </a>
         </div>
         <div class="slider">
             <ul id="lightSlider">
