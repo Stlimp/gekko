678e95055312a8b4cc88b6bf280ec91b5f13a36f
St.limp <st.limp@mail.ru>
2016-08-19 02:43:40 +0300

    Index page redesign continued

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,106 @@ $this->registerJsFile('@web/js/lightslider.min.js', ['depends' => [\yii\web\Jque
 $this->registerJsFile('@web/js/lightSlider.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->registerJsFile('@web/js/slide-up-down.js');
 ?>
+<style>
+    .left_third{
+        transition: all 1s ease;
+      -moz-transition: all 1s ease;
+      -ms-transition: all 1s ease;
+      -webkit-transition: all 1s ease;
+      -o-transition: all 1s ease;
+    }
+    .left_third:hover{
+   /*      transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    -o-transform: scale(1.1);
+    -ms-transform: scale(1.1); IE 9
+    -ms-filter: "progid:DXImageTransform.Microsoft.Matrix(M11=1.1, M12=0, M21=0, M22=1.1, SizingMethod='auto expand')"; IE8
+     filter: progid:DXImageTransform.Microsoft.Matrix(M11=1.1, M12=0, M21=0, M22=1.1, SizingMethod='auto expand'); IE6 and 7  */
+    }
+    .overflow{
+        
+        display:block;
+        position:relative;
+    }
+    .left_third{
+        display:block !important;
+            position: relative;
+            text-align: left;
+    }
+    .left_third:after {
+        
+        position:absolute;
+        opacity:0;
+        transition: all 0.5s;
+        -webkit-transition: all 0.5s;
+        content:'\A';
+        width:100%; height:100%;
+        top:0; left:0;
+        background:rgba(0,0,0,0.3);
+    }
+    .left_third:hover:after    {
+        opacity:1;
+    }
+    .left_third>a>img{
+        width: 100%;
+        height: 100%;
+    }
+    .left_third>a{
+        display: block;
+        width: 100%;
+        height: 100%;
+    }
+    .left_third p{
+        padding:10px;
+        z-index: 2;
+        color:white;
+        font-size: 40px;
+        text-shadow: 2px 0 4px #222;
+        letter-spacing: .03em;
+    }
+    .left_third img{
+        width:100%;
+        height:100%;
+    }
+
+    
+    .right_third p{
+        padding:10px;
+        z-index: 2;
+        color:white;
+        text-shadow: 2px 0 4px #222;
+        letter-spacing: .03em;
+    }
+    .right_third span{
+        z-index: 2;
+        color:white;
+    }
+    .right_split:after {
+        
+        position:absolute;
+        opacity:0;
+        transition: all 0.5s;
+        -webkit-transition: all 0.5s;
+        content:'\A';
+        width:100%; height:100%;
+        top:0; left:0;
+        background:rgba(0,0,0,0.3);
+    }
+    .right_split:hover:after    {
+        opacity:1;
+    }
+
+    .test{
+        background-image: url(../web/images/content/test_background.png);
+        
+    }
+    .cart{
+        background-image: url(../web/images/content/cart_background.png);
+    }
+
+
+</style>
 
 <div id="upper"></div>
 <!--<section>!-->
@@ -14,78 +114,94 @@ $this->registerJsFile('@web/js/slide-up-down.js');
   
 <div class="site-index">
     <div class="jumbotron">
-        <div class="col-md-12 hover-slide text-center" id="scroll" style="height:0px;text-align:right;bottom: -100px;";> <a href="#upper" ><div class="button-up"></div> </a>   </div>     
-        <div class="page-text">
-        <a href="?ProductColorSearch%5Bproduct_subcategory_name%5D=<?= $product->product_product_name?>&r=productcolor%2Findex">
-            <div class="left_third" style= <?= "background-image:url(&quot;".$product->product_product_image."&quot;);background-size:cover" ?>>
-                <?php
-                    $words = explode(" ", $product->product_product_name);
-                    $font_class = 'left_product_name';
-                    foreach ($words as $word) {
-                        if(mb_strlen($word)>9){
-                            $font_class = 'left_product_name_small';
-                        }
-                    }
 
-                   
-                ?>
-                <span class="<?= $font_class?>" >
-                    <?= mb_strtoupper($product->product_product_name) ?>
-                </span>
-            </div>
+        
+            <div class="col-md-12 hover-slide text-center" id="scroll" style="height:0px;text-align:right;bottom: -60px;";> <a href="#upper" ><div class="button-up"></div> </a>   </div>     
+        <div class="page-header" style="margin-top:70px;">ДЕКОРАТИВНЫЙ КАМЕНЬ <b>GEKKOSTONE</b></div>
+        <div class="page-text" style="margin-top:0px;">
+
+        <!--<a href="?ProductColorSearch%5Bproduct_subcategory_name%5D=<?= $product->product_product_name?>&r=productcolor%2Findex">
+             <div class="left_third" style= <?= "background-image:url(&quot;".$product->product_product_image."&quot;);background-size:cover" ?>>-->
+               <?php
+                   $words = explode(" ", $product->product_product_name);
+                   $font_class = 'left_product_name';
+                   foreach ($words as $word) {
+                       if(mb_strlen($word)>9){
+                           $font_class = 'left_product_name_small';
+                       }
+                   }
+           
+                  
+               ?>
+            <a href="google.com">
+                <div class="left_third" style="padding:0;">
+                    <p style="font-weight:900 !important;position: absolute;top:75%;transform: translateY(-50%);"><?= mb_strtoupper($product->product_product_name) ?></p>
+                    <p style="font-size:25px;font-weight:100 !important;position: absolute;top:10%;transform: translateY(-50%);">Декоративный камень</p>
+                    <img src="http://www.eldoradostone.com/wp-content/uploads/2015/10/brick-thumb.jpg">                        
+                </div>
+            </a>
+           <!--     <span class="<?= $font_class?>" >
+                <?= mb_strtoupper($product->product_product_name) ?>
+            </span> -->
+<!--             </div> -->
+
         </a>
+        <div class="right_third">
+            <a href="#">
+                <div class="right_split test" style="top:0">
+                    <span style="font-weight:900 !important;position: absolute;top:30%;transform: translateY(-50%);font-family:Corbel;" class="test_text">НАТУРАЛЬНЫЙ ИЛИ ИСКУСТВЕННЫЙ?</span>
+                    <p style="position: absolute;top:75%;transform: translateY(-50%); font-family:Corbel"><b>ТЕСТ.</b> Попробуй отыскать натуральные камни среди искусственных камней GEKKOSTONE!</p>
+                </div>
+            </a>
+            <a href="index.php?r=site%2Fcart">
+                <div class="right_split cart" style="bottom:0">
+                    <p style="position: absolute;top:30%;transform: translateY(-50%);font-family:Corbel">Собери собственную галерею из наиболее понравившихся фактур 
+    и расцветок!</p>
+                    <span class="test_text" style="position: absolute;top:50%;transform: translateY(-50%); font-size:35px; font-family:Corbel;">МОЯ ГАЛЕРЕЯ</span>
+                    <p style="position: absolute;top:75%;transform: translateY(-50%); font-family:Corbel">Расчет необходимого количества камня, его веса и стоимости!</p>
+                </div>
+            </a>
+        </div>
         <div class="center_third">
             <div class="center_split">
-               <a href="#">
-                    <div class="center_split_vertically">
-                       <p>ПОДБОР КАМНЯ</p>
-                    </div>
-                </a>
+               
                <div class="center_split_text"><p>При помощи конфигуратора «ПОДБОР КАМНЯ» 
                     Вы сможете самостоятельно подобрать самую 
                     оптимальную фактуру и расцветку декоративного
                     камня для интерьера или экстерьера Вашего дома.</p>
                 </div>
+                <a href="#">
+                    <div class="center_split_vertically">
+                       <p>ПОДБОР КАМНЯ</p>
+                    </div>
+                </a>
             </div>
             
             <div class="center_split">
-                <a href="#">
-                <div class="center_split_vertically">
-                   <p>ДО И ПОСЛЕ</p>
-                </div>
-                </a>    
+                  
                 <div class="center_split_text"><p>В фотогалерее представлены фотографии интерьеров
                         и экстерьеров ДО и ПОСЛЕ проведения облицовочных 
                         работ по укладке декоративного камня.</p> 
                 </div>
+                <a href="#">
+                <div class="center_split_vertically">
+                   <p>ДО И ПОСЛЕ</p>
+                </div>
+                </a>  
             </div>
                 <div class="center_split" style="margin-bottom:0px">
-                    <a href="index.php?r=videogallery%2Findex"><div class="center_split_vertically" style="background-image:url(images/content/videogallery.jpg);background-size:cover;">
-                       <p>ВИДЕОГАЛЕРЕЯ</p>
-                   </div></a>
+                    
                    <div class="center_split_text"><p>В видеогалерее представлены демонстрационные,
                     а также обучающие видео по применению в облицовке
                     декоративного облицовочного камня.</p>
                     </div>
+                    <a href="index.php?r=videogallery%2Findex"><div class="center_split_vertically" style="background-image:url(images/content/videogallery.jpg);background-size:cover;">
+                       <p>ВИДЕОГАЛЕРЕЯ</p>
+                   </div></a>
                 </div>
             </a>
         </div>
-        <div class="right_third">
-            <a href="#">
-                <div class="right_split">
-                    <span class="test_text">НАТУРАЛЬНЫЙ ИЛИ ИСКУСТВЕННЫЙ?</span>
-                    <p style="font-family:Corbel"><b>ТЕСТ.</b> Попробуй отыскать натуральные камни среди искусственных камней GEKKOSTONE!</p>
-                </div>
-            </a>
-            <a href="index.php?r=site%2Fcart">
-                <div class="right_split">
-                    <p style="font-family:Corbel">Собери собственную галерею из наиболее понравившихся фактур 
-    и расцветок!</p>
-                    <span class="test_text">МОЯ ГАЛЕРЕЯ</span>
-                    <p style="font-family:Corbel">Расчет необходимого количества камня, его веса и стоимости!</p>
-                </div>
-            </a>
-        </div>
+        
         <div class="slider">
             <ul id="lightSlider">
                 <?php foreach ($photos as $image)  { ?>
@@ -94,6 +210,78 @@ $this->registerJsFile('@web/js/slide-up-down.js');
                 </li>
                 <?php } ?>
             </ul>
+        </div>
+
+        <div class="bottom_div1">
+            <a href="index.php?r=site%2Fadvantages">
+                <div class="quarter">
+                        <div class="header">ОПТИМАЛЬНЫЙ ВЫБОР</div>
+                        <div class="text">Преимущества декоративно-облицовочного камня GEKKOSTONE</div>
+                        <img src="images/content/test_background.png" alt="">
+                </div>
+            </a>
+            <a href="index.php?r=faq%2Findex">
+                <div class="quarter">
+                        <div class="header">FAQ: ВОПРОС-ОТВЕТ</div>
+                        <div class="text">Ответы на наиболее часто задаваемые
+                                        вопросы по подбору, монтажу и после-
+                                        дующей эксплуатации декоративного
+                                        облицовочного камня.
+                        </div>
+                        <img src="images/content/test_background.png" alt="">  
+                </div>
+            </a>
+            <a href="index.php?r=videogallery%2Findex">
+                <div class="quarter">
+                    <div class="header">ВИДЕОГАЛЕРЕЯ</div>
+                    <div class="text">Видеоролики по монтажу и расшивке 
+                                    швов декоративного облицовочного 
+                                    камня GEKKOSTONE.
+                    </div>
+                    <img src="images/content/test_background.png" alt="">
+            </div>
+            </a>
+            <a href="index.php?r=site%2Frocklaying">
+            <div class="quarter">
+                
+                    <div class="header">ИНСТРУКЦИЯ ПО УКЛАДКЕ КАМНЯ И РАСШИВКЕ ШВОВ</div>
+                    <div class="text">Инструкции по монтажу декоративного 
+                                    камня, расшивке швов, а также полезные 
+                                    советы по успешной последующей 
+                                    эксплуатации.
+                    </div>
+                    <img src="images/content/test_background.png" alt="">
+                
+            </div>
+            </a>
+        </div>
+        
+        <div class="iconblock2" style="display:inline-block;width:15%;float:left;text-align:left;">
+            <span><a href="#"><img src="images/icons/ok.png" alt="" onmouseover="this.src='images/icons/okhover.png';" onmouseout="this.src='images/icons/ok.png';"></a></span>
+            <span><a href="#"><img src="images/icons/vk.png" alt="" onmouseover="this.src='images/icons/vkhover.png';" onmouseout="this.src='images/icons/vk.png';"></a></span>
+            <span><a href="#"><img src="images/icons/facebook.png" alt="" onmouseover="this.src='images/icons/facebookhover.png';" onmouseout="this.src='images/icons/facebook.png';"></a></span>
+            <span><a href="#"><img src="images/icons/twitter.png" alt="" onmouseover="this.src='images/icons/twitterhover.png';" onmouseout="this.src='images/icons/twitter.png';"></a></span>
+            <span><a href="#"><img src="images/icons/youtube.png" alt="" onmouseover="this.src='images/icons/youtubehover.png';" onmouseout="this.src='images/icons/youtube.png';"></a></span>   
+        </div>
+        <div style="width:85%;float:right"> 
+            <hr style="margin-top: 5px;">
+        </div>
+
+        <div class="half">
+            <span>
+                <form>
+                    <div>email</div>
+                    <div class="input">
+                        <input type="text" class="button" id="email" name="email" placeholder="">
+                        <input type="submit" class="button" id="submit" value="ПОДПИСКА">
+                      </div>
+                </form>
+            </span>
+        </div>
+        <div class="half">
+            <div style="display:block;background-color:yellow;width:35%;height:100%;float:right"></div>
+            <div style="display:block;background-color:green;width:35%;height:100%;float:right;margin-right:10px"></div>        
+            <span style="float:right;padding-right:15px;">скачать <a href="">price</a></span>
         </div>
       </div>
     </div>
