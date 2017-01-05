163377bd84fb843d04631855ac6f442ae313e89a
St.limp <st.limp@mail.ru>
2017-01-06 01:10:39 +0300

    Cart script reimplemented

--- web/js/cart.js
+++ web/js/cart.js
@@ -58,7 +58,6 @@ function order() {
 	var current_product_color=document.getElementById("product_subcategory_name_".concat(color_id)).innerHTML;//название камня
 	$.get('index.php?r=product/get-products',function(data){;
         var data = $.parseJSON(data);
-        //console.log(data);
         for(var i = 0; i < data.length; i++)
 		 {
 		   if(data[i].product_product_name.toLowerCase() === current_product_color.toLowerCase())
@@ -71,95 +70,113 @@ function order() {
 		 }
     });
     function calculate(color_data){
+
     	var regular_value = parseFloat(document.getElementById("regular_input_".concat(color_id)).value);
     	var angular_value = parseFloat(document.getElementById("angular_input_".concat(color_id)).value);
-
     	if (isNaN(regular_value)){
     		regular_value=0;
     	}
     	if (isNaN(angular_value)){
     		angular_value=0;
     	}
-    	
-    	if (document.getElementById('add_five_percent_'.concat(color_id)).checked) {//. добавить 5%, если выставленна галка
-    		regular_value =regular_value*1.05;
-    		angular_value=angular_value*1.02;
-    	};
 
-    	
-    	
-		if (!document.getElementById('seamless_'.concat(color_id)).checked) {
-	    	var angular_packages=Math.ceil(angular_value/color_data.product_angular_calculation_size);//1. количество упаковок
-	    	var angular_square=angular_value/color_data.product_angular_calculation_size_square*color_data.product_angular_calculation_size;//2. площадь углов
 
-	    	if (document.getElementById('reduce_square_'.concat(color_id)).checked) {
-	    		var regular_square=(regular_value-angular_square);// . площадь рядовых при учете угловых		
+    	if (!document.getElementById('seamless_'.concat(color_id)).checked) {
+	    	if (document.getElementById('add_five_percent_'.concat(color_id)).checked) {
+	    		var angular_packages=Math.ceil(angular_value/color_data.product_angular_calculation_size*1.02);//1. количество упаковок
+	    	}
+	    	else{
+	    		var angular_packages=Math.ceil(angular_value/color_data.product_angular_calculation_size);//1. количество упаковок
+	    	}
+	    	angular_packages=Math.ceil(angular_packages.toFixed(3));
+	  		var angular_result=angular_packages*color_data.product_angular_calculation_size//4. подсчет погонных метров угловой
+			angular_result=angular_result.toFixed(3);
+
+	  		var weight_angular=angular_result*color_data.product_angular_weight;
+	  		var angular_square=angular_value/color_data.product_angular_calculation_size*color_data.product_angular_calculation_size_square;//2. площадь углов
+	  		angular_square=angular_square.toFixed(3)
+
+	  		if (document.getElementById('reduce_square_'.concat(color_id)).checked) {
+		    		var regular_square=(regular_value-angular_square);// . площадь рядовых при учете угловых		
+				}
+				else{
+					var regular_square=regular_value;//. площадь рядовых без учета угловых
 			}
-			else{
-				var regular_square=regular_value;//. площадь рядовых без учета угловых
-			}
+
 			
 
-			console.log(regular_square);
+			if (document.getElementById('add_five_percent_'.concat(color_id)).checked) {//. РґРѕР±Р°РІРёС‚СЊ 5%, РµСЃР»Рё РІС‹СЃС‚Р°РІР»РµРЅРЅР° РіР°Р»РєР°
+	    		regular_square =(regular_square*1.05).toFixed(3);
+	    		angular_square=(angular_square*1.02).toFixed(3);
+    		};
 			
-	    	var regular_packages=Math.ceil(regular_square/color_data.product_regular_calculation_size);//3. количество упаково рядовой
 
-	    	var angular_result=Math.ceil(angular_packages*color_data.product_angular_calculation_size*1000)/1000;//4. подсчет погонных метров угловой
-	    	var regular_result=Math.ceil(regular_packages*color_data.product_regular_calculation_size*1000)/1000;//5. подсчет погонных метров рядовой
-
-	    	document.getElementById('angular_result_'.concat(color_id)).innerHTML=angular_result;//. стоимость уголвых
-	    	document.getElementById('regular_result_'.concat(color_id)).innerHTML=regular_result;//. стоимость рядовых
-
-	    	//var price=Math.ceil((angular_result+regular_result)*color_data.product_price/100)*100;//6. общая цена до деноминации
-	    	var price=Math.ceil((angular_result+regular_result)*color_data.product_price*100)/100;//6. общая цена после деноминации
-
-	    }
-	    else{
-	    	/*Бесшовный монтаж*/
-	    	var angular_packages=Math.ceil(angular_value/color_data.product_angular_seamless_calculation_size);//1. количество упаковок
-	    	var angular_square=angular_value/color_data.product_angular_seamless_calculation_size_square*color_data.product_angular_seamless_calculation_size;//2. площадь углов
-
-	    	if (document.getElementById('reduce_square_'.concat(color_id)).checked) {
-	    		var regular_square=(regular_value-angular_square);// . площадь рядовых при учете угловых
-			}
-			else{
-				var regular_square=regular_value;//. площадь рядовых без учета угловых
-			}
-			console.log(regular_square);
+			var regular_packages=Math.ceil(regular_square/color_data.product_regular_calculation_size);//3. количество упаково рядовой
 			
-	    	var regular_packages=Math.ceil(regular_square/color_data.product_regular_seamless_calculation_size);//3. количество упаково рядовой
+			var regular_result=(regular_packages*color_data.product_regular_calculation_size).toFixed(3);//5. подсчет погонных метров рядовой
+			if (parseFloat(regular_result)<0){
+    			regular_result=0;
+    		}
 
-	    	var angular_result=Math.ceil(angular_packages*color_data.product_angular_calculation_size*1000)/1000;//4. подсчет погонных метров угловой
-	    	var regular_result=Math.ceil(regular_packages*color_data.product_regular_calculation_size*1000)/1000;//5. подсчет погонных метров рядовой
+			var weight_regular=regular_result*color_data.product_regular_weight;
+		}
+		else
+		{
+			if (document.getElementById('add_five_percent_'.concat(color_id)).checked) {
+	    		var angular_packages=Math.ceil(angular_value/color_data.product_angular_seamless_calculation_size*1.02);//1. количество упаковок
+	    	}
+	    	else
+	    	{
+	    		var angular_packages=Math.ceil(angular_value/color_data.product_angular_seamless_calculation_size);//1. количество упаковок
+	    	}
+	    	angular_packages=Math.ceil(angular_packages.toFixed(3));
+	  		var angular_result=angular_packages*color_data.product_angular_calculation_size;//4. подсчет погонных метров угловой
+	  		angular_result=angular_result.toFixed(3);
+	  		var weight_angular=angular_result*color_data.product_angular_weight;
+	  		var angular_square=angular_value/color_data.product_angular_seamless_calculation_size*color_data.product_angular_seamless_calculation_size_square;//2. площадь углов
+	  		angular_square=angular_square.toFixed(3)
 
-	    	document.getElementById('angular_result_'.concat(color_id)).innerHTML=angular_result;//. стоимость уголвых
-	    	document.getElementById('regular_result_'.concat(color_id)).innerHTML=regular_result;//. стоимость рядовых
+    		
+	  		if (document.getElementById('reduce_square_'.concat(color_id)).checked) {
+		    		var regular_square=(regular_value-angular_square);// . площадь рядовых при учете угловых		
+				}
+				else{
+					var regular_square=regular_value;//. площадь рядовых без учета угловых
+			}
+
+	  		if (document.getElementById('add_five_percent_'.concat(color_id)).checked) {//. РґРѕР±Р°РІРёС‚СЊ 5%, РµСЃР»Рё РІС‹СЃС‚Р°РІР»РµРЅРЅР° РіР°Р»РєР°
+	    		regular_square =(regular_square*1.05).toFixed(3);
+	    		angular_square=(angular_square*1.02).toFixed(3);
+    		};
+
+			var regular_packages=Math.ceil(regular_square/color_data.product_regular_seamless_calculation_size);//3. количество упаково рядовой
+;
+			var regular_result=(regular_packages*color_data.product_regular_calculation_size).toFixed(1);//5. подсчет погонных метров рядовой
+
+			var weight_regular=regular_result*color_data.product_regular_weight;
+
+		}
 
 
-	    	//var price=Math.ceil((angular_result+regular_result)*color_data.product_price_seamless/100)*100;//6. общая цена до деноминации
-	    	var price=Math.ceil((angular_result+regular_result)*color_data.product_price_seamless*100)/100;//6. общая цена после деноминации
+    	
 
+		document.getElementById('angular_result_'.concat(color_id)).innerHTML=angular_result;
+	    document.getElementById('regular_result_'.concat(color_id)).innerHTML=regular_result;
 
-	   	}
+	    
+	    var price=(parseFloat(angular_result)+parseFloat(regular_result))*parseFloat(color_data.product_price);
+	    var price=price.toFixed(2);
+	   	
 	
     	
-    	document.getElementById('result_color_'.concat(color_id)).innerHTML=addCommas(price);
+    	document.getElementById('result_color_'.concat(color_id)).innerHTML=price;
     	document.getElementById('price_color_'.concat(color_id)).value=price;
 
     	document.getElementById('regular_packages_input_'.concat(color_id)).value=regular_result;
     	document.getElementById('angular_packages_input_'.concat(color_id)).value=angular_result;
-    	/*console.log(Math.ceil(angular_value/color_data.product_angular_calculation_size));
-    	console.log(angular_value*color_data.product_angular_calculation_size_square);
-		console.log(regular_square);
-		console.log(regular_packages);*/
 
-
-    	//console.log(color_data.product_angular_calculation_size);
-		//console.log(color_data);
-
-		var weight_regular=regular_packages*color_data.product_regular_weight;
-		var weight_angular=angular_packages*color_data.product_angular_weight;
-		var weight=weight_regular+weight_angular;
+		
+		var weight=(weight_regular+weight_angular).toFixed(1);
 		document.getElementById('result_weight_'.concat(color_id)).innerHTML=weight;//. вес
 		document.getElementById('weight_color_'.concat(color_id)).value=weight;//. вес
 		document.getElementById('angular_result_'.concat(color_id)).value=weight;//. вес
@@ -185,9 +202,9 @@ function order() {
 			}
 	});
 	
-	document.getElementById("total_weight").innerHTML=weight_total;//Конечный вес
+	document.getElementById("total_weight").innerHTML=weight_total.toFixed(2);//Конечный вес
 
-	document.getElementById("price_value").innerHTML=addCommas(sum_total);//Конечная сумма 
+	document.getElementById("price_value").innerHTML=sum_total.toFixed(2);//Конечная сумма 
 
 	document.getElementById('price_input').value=sum_total; 
 	document.getElementById('weight_input').value=weight_total; 
